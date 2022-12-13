f44ece5c35aa77962f4d477cba42f114d66d81ff
y.zaytzeva <y.zaytzeva@fonmix.ru>
2022-12-13 14:15:24 +0300

    fix

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -4,10 +4,12 @@ function Addresses() {
     return (
         <div className='d-inline-flex flex-column gap-2 sheet p-5 flex-grow-1'>
             <div className='title mb-4'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
-                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <SecondaryButton>Добавить филиал</SecondaryButton>
+            <div className='d-inline-flex flex-grow-1 flex-column justify-content-between gap-3'>
+                <div>
+                    <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+                    <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+                </div>
+                <SecondaryButton className="mt-5">Добавить филиал</SecondaryButton>
             </div>
         </div>
     );
